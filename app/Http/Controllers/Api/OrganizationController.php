359239bab1ee291f1ad61f1189df744fe476623a
ildan2108 <ildannab2112@icloud.com>
2026-09-14 14:17:55 +0300

    feat: add organization parsing queue

--- app/Http/Controllers/Api/OrganizationController.php
+++ app/Http/Controllers/Api/OrganizationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateOrganizationRequest;
 use App\Http\Resources\OrganizationResource;
+use App\Jobs\ParseOrganization;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -23,7 +24,22 @@ class OrganizationController extends Controller
 
     public function update(UpdateOrganizationRequest $request): OrganizationResource
     {
-        $organization = $request->user()->organization()->updateOrCreate([], $request->validated());
+        $organization = $request->user()->organization()->updateOrCreate(
+            [],
+            [
+                'yandex_url' => $request->validated('yandex_url'),
+                'parsing_status' => 'pending',
+                'parsing_error' => null,
+                'parsing_started_at' => null,
+                'parsed_at' => null,
+                'name' => null,
+                'average_rating' => null,
+                'ratings_count' => null,
+                'reviews_count' => null,
+            ],
+        );
+
+        ParseOrganization::dispatch($organization);
 
         return new OrganizationResource($organization);
     }
