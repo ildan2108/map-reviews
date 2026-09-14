949e77b57ea6653695c375fce95578f9ff684c39
ildan <ildannab2112@icloud.com>
2026-09-14 14:27:27 +0300

    feat: Добавление организации

---
+++ app/Services/YandexMaps/YandexMapsParser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\YandexMaps;
+
+class YandexMapsParser
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
